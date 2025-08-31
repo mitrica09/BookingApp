2dd6498bf9a29e8d3c7ab24dbbb7423eec3c7cb1
Mitrica Alexandru <alexmitrica40@gmail.com>
2025-08-31 22:06:37 +0300

    Setarile proiectului si a bazei de date + login si register functional.

---
+++ BookingApp/ViewModels/Account/VerifyEmailVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingApp.ViewModels.Account
+{
+    public class VerifyEmailVM
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
